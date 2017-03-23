f942f9a2a041c9a3885d12b243ed0ae8720476a6
Lee <1258963839@qq.com>
2017-03-23 18:00:17 +0800

    模块：test

--- tp5/application/route.php
+++ tp5/application/route.php
@@ -9,14 +9,37 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
+// use think\Route;
+
+// Route::rule('hello/:name', 'admin/userListDate/hello');
+// 定义闭包
+// Route::rule('hello/:name', function ($name) {
+//     return 'Hello '.$name.'!';
+// });
+
 return [
+    // 全局变量规则定义
     '__pattern__' => [
         'name' => '\w+',
+        'city' => '\w+',
+        'year' => '\d{4}',
+        'month' => '\d{2}',
     ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
+    // 路由规则定义
+    // '[userList]'   => [
+    //     ':city'   => ['admin/userListDate/userList', ['method' => 'get'], ['city' => '\w+']],
+    //     ':name' => ['admin/userListDate/userList', ['method' => 'post'], ['name' => '\w+']],
+    // ],
     // 添加路由规则，路由到UserListDate控制器的hello操作方法
-    'hello/[:name]' => 'admin/userListDate/hello',
+    // 当路由规则以$结尾的时候就表示当前路由规则需要完整匹配
+    // 'hello/[:name]$' => 'admin/userListDate/hello',
+    
+    // 定义闭包,闭包函数的参数就是路由规则中定义的变量
+    // 'hello/[:name]' => function ($name) {
+    //     return 'Hello '.$name.'!';
+    // },
+
+    // 'blog/:year/:month' => ['blog/archive', ['method' => 'get'], ['year' => '\d{4}', 'month' => '\d{2}']],
+    // 'blog/:id'          => 'blog/get',
+    // 'blog/:name'        => 'blog/read',
 ];
