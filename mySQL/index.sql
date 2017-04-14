f1f5d810247c5f72a7d0f2c2ff77dfa2b1a769cd
Lee <1258963839@qq.com>
2017-04-14 17:53:36 +0800

    MySQL基础4/14

---
+++ mySQL/index.sql
@@ -0,0 +1,8 @@
+-- 原生MySQL语句
+-- MySQL语句的规范：1.关键字与函数名称全部大写 2.数据库名称、表名称、字段名称全部小写 3.SQL语句必须以分号结尾';'
+-- 修改MySQL提示符：PROMPT \u\h \d> == root@localhost (none)>
+-- 创建数据库语法： CREATE {DATABASE | SCHEMA} [IF NOT EXISTS] db_name [DEFAULT] CHHARACTER SET [=] charset_name
+-- 查看当前服务器下的数据库列表： SHOW {DATABASE | SCHEMAS} [LIKE 'pattern' | WHERE expr]
+-- 删除数据库：DROP {DATABASE | SCHEMA} [IF EXISTS] db_name
+
+-- 数据类型之整型、浮点型
